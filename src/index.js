505938044d7dd4433ccf4f7c522dd410d018574e
mukeshutmani <kumarmukesh789123@gmail.com>
2024-08-22 18:22:12 +0500

    add database connection

---
+++ src/index.js
@@ -0,0 +1,48 @@
+// require('dotenv').config({path: '/env'})
+import express from 'express'
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path:'./env'
+})
+
+connectDB()
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from 'express'
+const app = express()
+ 
+;(async () => {
+  try {
+   await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+   app.on("error", (error)=> {
+    console.log("ERROR: ",error);
+    throw error
+   })
+
+   app.listen(process.env.PORT, ()=> {
+    console.log(`APP is listening on port: ${process.env.PORT}`);
+
+   })
+
+  } catch (error) {
+    console.error("Error: DateBase Connection",error)
+    throw error
+  }
+})();
+
+*/
