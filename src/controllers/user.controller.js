cc534c0e4c587c46ab54fdfaecd49cf54acb9993
mukeshutmani <kumarmukesh789123@gmail.com>
2024-09-01 11:25:26 +0500

    addd routes for usercontroller methods

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,7 +5,7 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
-import { json } from "express";
+
 
 const generateAccessTokenAndRefeshToken = async (userId) => {
   try {
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,20 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { 
+    
+    changeCurrentPassword, 
+    getCurrentUser, 
+    getUserChannelProfile, 
+    getWatchHistory, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    registerUser, 
+    updateAccountDetail, 
+    updateUserAvatar, 
+    updateUserCoverImage 
+
+} from "../controllers/user.controller.js";
+
 import { upload } from "../middlewares/multer.middleware.js";
 import { verfiyJWT } from "../middlewares/auth.middleware.js";
 
@@ -26,9 +41,17 @@ router.route('/login').post(loginUser)
 
  // secure routes
  router.route("/logout").post(verfiyJWT ,logoutUser)
-
  router.route("/refresh-token").post(refreshAccessToken)
+ router.route("/change-password").post(verfiyJWT, changeCurrentPassword)
+ router.route("/current-user").get(verfiyJWT, getCurrentUser)
+ router.route("/update-account").patch(verfiyJWT, updateAccountDetail)
 
+ router.route("/avatar").patch(verfiyJWT, upload.single("avatar"), updateUserAvatar)
+ router.route("/cover-image").patch(verfiyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+ router.route("/c/:username").get(verfiyJWT, getUserChannelProfile)
+
+ router.route("/history").get(verfiyJWT, getWatchHistory)
 
 // http://localhost:8000/users/regitser
 export default router
